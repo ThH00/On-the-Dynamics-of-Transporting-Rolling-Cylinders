6c63258424456281c2a1c7e18cce0fb9549dabf3
Theresa Honein <terryhonein@gmail.com>
2024-05-13 13:30:52 -0700

    edits

--- get-initial-configuration/obtaining_initial_configs.m
+++ get-initial-configuration/obtaining_initial_configs.m
@@ -1,18 +1,4 @@
-%% looping over different cylinder dimensions
-d = [9, 9, 9.25]/12;    % dimater, ft
-r = d/2;                % calculating radius, ft
-h = [51, 51, 55]/12;    % heigt, ft
-m = [113, 117, 139];    % lb
-theta = pi/10;
-
-n = length(d);
-I_xbar = zeros(3,n);
-II_xbar = zeros(3,n);
-for i = 1:n
-    [I_xbar(:,i),II_xbar(:,i)] = get_config(r(i),h(i),theta);
-end
-
-%% looping over different angles theta
+% looping over different angles theta
 r = 9/12/2;         % radius, ft
 h = 51/12;          % height, ft
 m = 113;            % mass, ft
